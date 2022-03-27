2300f72d9212dd249d3f26f0670150081ee01c80
Ami-hub <tnhh952@gmail.com>
2022-03-27 14:50:34 +0300

    finish Ex4
    add tests and the implementaion for camera

--- unittests/geometries/GeometriesTests.java
+++ unittests/geometries/GeometriesTests.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
- * Unit tests for geometries.Cylinder class
+ * Unit tests for geometries.Geometries class
  *
  * @author Amitay Cahalon && Daniel Wolpert
  */
@@ -27,7 +27,7 @@ public class GeometriesTests {
         Sphere sphere = new Sphere(new Point(-2, 0, 0), 1);
         Plane plane = new Plane(new Point(-6, -2, 0), new Vector(0, 0, 1));
         Geometries geometries = new Geometries(triangle, sphere, plane);
-        List<Point> result1 = geometries.findIntersections(new Ray(new Point(-2, 0, 0),
+        List<Point> result1 = geometries.findIntersections(new Ray(new Point(-2, 0, 3),
                 new Vector(0, 0, -1)));
         assertEquals(3, result1.size(), "Wrong number of points");
 
@@ -36,18 +36,20 @@ public class GeometriesTests {
         Geometries geometries1 = new Geometries();
         assertNull(geometries1.findIntersections(new Ray(new Point(0, 2, 0), new Vector(0, 1, 0))),
                 "Intersections found on an empty collection");
+
         // T12: no shape intersects with a body
         assertNull(geometries.findIntersections(new Ray(new Point(0, 0, 5), new Vector(1, 0, 0))),
                 "Intersections found");
+
         // T13: only one shape intersects
         Geometries geometries2 = new Geometries(triangle, sphere);
-        List<Point> result2 = geometries2.findIntersections(new Ray(new Point(-2, 0, 0),
+        List<Point> result2 = geometries2.findIntersections(new Ray(new Point(-2, 0, 3),
                 new Vector(0, 0, -1)));
         assertEquals(2, result2.size(), "Wrong number of points");
-        // T14: all shapes intersects
 
-        Geometries geometries3 = new Geometries(triangle, new Sphere(new Point(0.5, 0, 0), 1));
-        List<Point> result3 = geometries3.findIntersections(new Ray(new Point(1, 0, 0),
+        // T14: all shapes intersects
+        Geometries geometries3 = new Geometries(triangle, new Sphere(new Point(2, 0, 0), 1));
+        List<Point> result3 = geometries3.findIntersections(new Ray(new Point(2, 0.25, 4),
                 new Vector(0, 0, -1)));
         assertEquals(3, result3.size(), "Wrong number of points");
     }
